2fbdc4e73c4cba6997c65d142c732823a647ac5a
Skrillx13 <skrillx1300@gmail.com>
2025-02-26 21:57:14 +0800

    Added some new styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="content-container">
+    <h1>Hi!</h1>
+    <p>
+        I'm Jonathan, username jnef13 on some spaces. I'm an aspiring software engineer/developer/computer science guy? I think... Anyways I'm
+        really good at static site generation, Python & JavaScript frameworks.
+    </p>
+    <i class="bi bi-0-circle"></i>
+</div>
